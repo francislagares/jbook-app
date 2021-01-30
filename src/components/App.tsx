f10bc38ec1a2e708e487163d770385f98e2a0815
Francis Lagares <francis.lagares@gmail.com>
2021-01-30 15:14:33 +0100

    feat: implement in-browser bundling plugin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import React, { useEffect, useRef, useState } from 'react';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { fetchPlugin } from '../plugins/fetch-plugin';
 
 const App = (): JSX.Element => {
-  return <h1>Hi!</h1>;
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.34/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 export default App;

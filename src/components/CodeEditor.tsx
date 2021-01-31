703c4086002b474c571fb60d81aaad2fafa5ed09
Francis Lagares <francis.lagares@gmail.com>
2021-01-31 18:43:24 +0100

    feat: add prettier to Editor component

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -3,9 +3,14 @@ import MonacoEditor, {
   OnChange,
   OnMount,
 } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import { useRef } from 'react';
 
 const CodeEditor: React.FC<EditorProps> = ({ defaultValue }) => {
+  const editorRef = useRef<any>();
   const onEditorDidMount: OnMount = editor => {
+    editorRef.current = editor;
     editor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
@@ -13,8 +18,24 @@ const CodeEditor: React.FC<EditorProps> = ({ defaultValue }) => {
     console.log('here is the current model value:', value, event);
   };
 
+  const prettierFormat = () => {
+    // Get current value from editor
+    const unformatted = editorRef.current.getModel().getValue();
+    // Format the value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    // Set the formated value back to the editor
+    editorRef.current.setValue(formatted);
+  };
+
   return (
     <>
+      <button onClick={prettierFormat}>Format</button>
       <MonacoEditor
         onMount={onEditorDidMount}
         onChange={handleEditorChange}

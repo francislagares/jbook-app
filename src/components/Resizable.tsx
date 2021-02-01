56cc40362ff7abbc958473a240c859a98b2cc7a1
Francis Lagares <francis.lagares@gmail.com>
2021-02-01 17:11:11 +0100

    feat: create Resizable component

---
+++ src/components/Resizable.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import './Resizable.css';
+
+interface ResizableProps {
+  direction: 'horizontal' | 'vertical';
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  let resizableProps: ResizableBoxProps;
+
+  useEffect(() => {
+    const listener = () => {
+      console.log(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 30],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
+};
+
+export default Resizable;

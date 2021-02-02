32e1055b9f18bb3997326b3ea874f6e3922ed202
Francis Lagares <francis.lagares@gmail.com>
2021-02-02 18:40:04 +0100

    style: apply dark theme to markdown editor

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -7,6 +7,7 @@ import './TextEditor.css';
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -28,14 +29,16 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor />
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={v => setValue(v || '')} />
       </div>
     );
   }
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={'# Header'} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };

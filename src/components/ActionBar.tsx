4b34fcf47a5749a2b1700b5870afb5f2966f124c
Francis Lagares <francis.lagares@gmail.com>
2021-02-04 19:33:55 +0100

    style: add icon buttons and css styling

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,14 +1,37 @@
 import { IActionBarProps } from '../interfaces';
 import { useActions } from '../hooks/useActions';
+import './ActionBar.css';
 
 const ActionBar: React.FC<IActionBarProps> = ({ id }) => {
   const { moveCell, deleteCell } = useActions();
 
+  // TODO: Create a reusable button component
   return (
-    <div>
-      <button onClick={() => moveCell(id, 'up')}>UP</button>
-      <button onClick={() => moveCell(id, 'down')}>DOWN</button>
-      <button onClick={() => deleteCell(id)}>DELETE</button>
+    <div className="action-bar">
+      <button
+        className="button is-primary is-small"
+        onClick={() => moveCell(id, 'up')}
+      >
+        <span className="icon">
+          <i className="fas fa-arrow-up"></i>
+        </span>
+      </button>
+      <button
+        className="button is-primary is-small"
+        onClick={() => moveCell(id, 'down')}
+      >
+        <span className="icon">
+          <i className="fas fa-arrow-down"></i>
+        </span>
+      </button>
+      <button
+        className="button is-primary is-small"
+        onClick={() => deleteCell(id)}
+      >
+        <span className="icon">
+          <i className="fas fa-times"></i>
+        </span>
+      </button>
     </div>
   );
 };
--- src/components/CellListItem.tsx
+++ src/components/CellListItem.tsx
@@ -2,22 +2,30 @@ import { ICellListItemProps } from '../interfaces';
 import CodeCell from '../components/CodeCell';
 import TextEditor from '../components/TextEditor';
 import ActionBar from '../components/ActionBar';
+import './CellListItem.css';
 
 const CellListItem: React.FC<ICellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
 
   if (cell.type === 'code') {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
   }
 
-  return (
-    <div>
-      <ActionBar id={cell.id} />
-      {child}
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;

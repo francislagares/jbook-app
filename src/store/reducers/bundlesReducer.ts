e7b21ede12a0401ca477f1b8c58ae6a21ce4f7de
Francis Lagares <francis.lagares@gmail.com>
2021-02-07 19:07:30 +0100

    refactor: re-define bundles reducer

--- src/store/reducers/bundlesReducer.ts
+++ src/store/reducers/bundlesReducer.ts
@@ -7,8 +7,19 @@ const initialState: IBundleState = {};
 const reducer = produce(
   (state: IBundleState = initialState, action: Action): IBundleState => {
     switch (action.type) {
-      case ActionType.BUNDLE_CREATED:
-        state[action.payload.cellId] = action.payload.bundle;
+      case ActionType.BUNDLE_START:
+        state[action.payload.cellId] = {
+          loading: true,
+          code: '',
+          error: '',
+        };
+        return state;
+      case ActionType.BUNDLE_COMPLETE:
+        state[action.payload.cellId] = {
+          loading: false,
+          code: action.payload.bundle.code,
+          error: action.payload.bundle.error,
+        };
         return state;
       default:
         return state;
